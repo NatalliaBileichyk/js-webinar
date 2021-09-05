8ce73fc9f6d73ae2e318377b221af20b6d40a138
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-09-05 21:18:47 +0300

    Changing Changing fibonacci.js according to the comment

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -15,20 +15,19 @@ function fibonacci(n) {
      * Also take into consideration the documentation of the function!
      */
     // PLACE YOUR CODE BETWEEN THIS...
-    
-let firstSum = 0;
-let secondSum = 1;
-if (n >= 0) {
+ 
+if (n < 0) {
+    nThFibonacci = 0;
+} else { 
+    let firstSum = 0;
+    let secondSum = 1;    
     while (n >= 1) {
-        nThFibonacci = firstSum + secondSum
-        secondSum = firstSum 
-        firstSum = nThFibonacci
-        n--;
+             nThFibonacci = firstSum + secondSum
+             secondSum = firstSum 
+             firstSum = nThFibonacci
+             n--;
     }
-} else {
-      nThFibonacci = 0
   };
-
     // ...AND THIS COMMENT LINE!
     return nThFibonacci;
 }
