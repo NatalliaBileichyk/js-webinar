43feb518074b1f62941b0617d145e84cc8fb4c58
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-11-10 08:59:22 +0300

    Solution to module-4/arraySum.js

--- module-4/arraySum.js
+++ module-4/arraySum.js
@@ -6,3 +6,22 @@
  * @param {Array} elements
  * @returns {number} summary of all integers or 0 in other cases
  */
+function arraySum (elements){
+    let sum = 0;
+
+    for (let i in elements){ 
+        if (typeof elements[i] == 'number'){
+            sum += elements[i]
+        } 
+        
+        if (typeof elements[i] == 'object'){
+            sum += arraySum (elements[i])
+        } else {
+            sum
+        }
+    }
+        
+    return sum;
+}
+
+module.exports = arraySum;
