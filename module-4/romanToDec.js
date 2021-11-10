cfc2f7584e2ae41c1d93fa2457d62ac9ac86f994
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-11-10 20:44:43 +0300

    Solution to module-4/romanToDec.js

--- module-4/romanToDec.js
+++ module-4/romanToDec.js
@@ -7,3 +7,27 @@
  * @returns {number} the Arabic (decimal) equivalent of the parameter
  * @throws Error in case of invalid input
  */
+function romanToDec (roman){
+   if (roman === "I" ||  roman === "i"){
+       return 1
+   } 
+   if (roman === "V" ||  roman === "v"){
+       return 5
+   } 
+   if (roman === "X" ||  roman === "x"){
+       return 10
+   }
+   if (roman === "L" ||  roman === "l"){
+       return 50
+   } 
+   if (roman === "D" ||  roman === "d"){
+       return 500
+   } 
+   if (roman === "M" ||  roman === "m"){
+       return 1000
+   } 
+      
+   throw new Error ("It doesn't have Arabic (decimal) equivalent")
+}
+
+module.exports = romanToDec;
