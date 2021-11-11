2da790f9515ccdcca09ff268ae77a2d3e5999b30
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-11-11 20:34:29 +0300

    module-4/longestString.js

--- module-4/longestString.js
+++ module-4/longestString.js
@@ -6,3 +6,31 @@
  * @param {Array.<string>} strings
  * @returns {string} longest string or empty string in other cases
  */
+function longestString (strings){
+    let longest = "";
+        
+    if (typeof strings === "object") {
+        let newArray = strings.filter(el => typeof el === 'string' || typeof el === 'object');
+            
+            for (let i in newArray){
+                if (typeof newArray[i] === 'object'){
+                   longestString(newArray[i])
+                }
+                
+                if (longest.length < newArray[i].length){
+                    longest = newArray[i]
+                } else if (longest.length === newArray[i].length){
+                    let sortArray = [longest, newArray[i]];
+                    longest = sortArray.sort()[0];
+                } else {
+                    longest = longest
+                }
+            }
+    } else {
+        longest = ""
+    }
+    
+    return longest;
+}
+
+module.exports = longestString;
